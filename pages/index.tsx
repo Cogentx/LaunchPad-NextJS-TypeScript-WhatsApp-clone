318a1bc4c8b7169583f129980f98731fee6150a5
Cogentx <code@cogentx.com>
2022-04-03 01:24:53 -0230

    Sidebar Comp (1) - setup and connect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Sidebar from '../components/Sidebar';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -12,7 +13,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <Sidebar/>
+      </main>
     </div>
   );
 };
