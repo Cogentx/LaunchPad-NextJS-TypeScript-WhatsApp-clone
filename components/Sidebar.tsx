d41d63ea09434a783c1991ed7cccf6b7acc4e22d
Cogentx <code@cogentx.com>
2022-04-03 02:05:11 -0230

    Sidebar (2) - profile image and icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,20 @@
+import { DotsVerticalIcon, AnnotationIcon } from '@heroicons/react/solid';
+
 export default function Sidebar() {
   return (
     <div>
-      <h3>Sidebar</h3>
+      <header className="flex items-center justify-between">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://avatars.githubusercontent.com/u/48112040?s=96&v=4"
+          alt="Profile Picture"
+          className="h-16 w-16 rounded-full border-[2px]"
+        />
+        <div className="flex items-center justify-end space-x-4">
+          <AnnotationIcon className="sbBtn" />
+          <DotsVerticalIcon className="sbBtn" />
+        </div>
+      </header>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import Sidebar from '../components/Sidebar';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
