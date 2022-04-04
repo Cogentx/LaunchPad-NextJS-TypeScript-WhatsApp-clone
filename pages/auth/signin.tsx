185f190ae53be545e3a6e85b72e9e34f7a088e7c
Cogentx <code@cogentx.com>
2022-04-04 19:15:10 -0230

    Refactor and Style Login Page Logo

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -8,18 +8,19 @@ export default function Signin() {
         <title>Login</title>
       </Head>
 
-      <main>
+      <main className="mt-20 flex min-h-screen flex-col items-center py-2 px-14">
         {/* Logo */}
-        <section className="flex flex-col">
-          <Image src="/WhatsAppLogo.png" alt="Company Logo" width={200} height={200} layout="fixed" className="" />
-          <button className="">Sign in with Google</button>
+        <section className="relative flex h-[200px] w-[200px] items-center">
+          <Image src="/WhatsAppLogo.png" alt="WhatsApp Logo" layout="fill" objectFit="contain" priority />
         </section>
 
         {/* Message */}
         <section></section>
 
         {/* Signin Button */}
-        <section></section>
+        <section>
+          <button className="font-semibold uppercase">Sign in with Google</button>
+        </section>
       </main>
     </>
   );
