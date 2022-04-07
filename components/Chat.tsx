fea79987b326c1bed500173eca77be66f17c3d6c
Cogentx <code@cogentx.com>
2022-04-07 17:28:30 -0230

    Chat - circle w/ first letter of email when no pic

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -13,24 +13,25 @@ export default function Chat({ id, users }: IProps) {
   const [user] = useAuthState(auth);
   const recipientEmail = getRecipientEmail(users, user?.email as string);
 
-  const recipientQuery = query(collection(db, users_url),where('email',"==",recipientEmail));
+  const recipientQuery = query(collection(db, users_url), where('email', '==', recipientEmail));
   const [recipientSnapshot] = useCollection(recipientQuery);
 
-  const photoURL = recipientSnapshot?.docs.map((user: any) => user.data())[0].photoURL;
-
-  console.log({photoURL});
-
+  const recipient = recipientSnapshot?.docs.map((user: any) => user.data())[0];
 
 
   return (
     <li>
       <div className="flex cursor-pointer items-center break-words p-4 hover:bg-[#e9eaeb]">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={photoURL}
-          alt="Profile Picture"
-          className="m-2 mr-4 h-10 w-10 rounded-full"
-        />
+        {recipient && recipient.photoURL ? (
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={recipient.photoURL} alt="Profile Picture" className="m-2 mr-4 h-10 w-10 rounded-full" />
+          </div>
+        ) : (
+          <div className="m-2 mr-4 h-10 w-10 flex items-center justify-center bg-gray-500 text-white rounded-full">
+            <p className="uppercase text-sm">{recipientEmail[0]}</p>
+          </div>
+        )}
         <p>{recipientEmail}</p>
       </div>
     </li>
