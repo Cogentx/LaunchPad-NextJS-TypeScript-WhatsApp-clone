8bb44aa99b06c28bb9e8ccfa223ac167f7aa69a4
Cogentx <code@cogentx.com>
2022-04-07 16:28:13 -0230

    Chat Comp (2) - include actual user profile pics

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,5 +1,7 @@
+import { collection, query, where } from 'firebase/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { auth, db, users_url } from '../firebase';
 import { getRecipientEmail } from '../lib/utils/getRecipientEmail';
 
 type IProps = {
@@ -9,15 +11,23 @@ type IProps = {
 
 export default function Chat({ id, users }: IProps) {
   const [user] = useAuthState(auth);
-
   const recipientEmail = getRecipientEmail(users, user?.email as string);
+
+  const recipientQuery = query(collection(db, users_url),where('email',"==",recipientEmail));
+  const [recipientSnapshot] = useCollection(recipientQuery);
+
+  const photoURL = recipientSnapshot?.docs.map((user: any) => user.data())[0].photoURL;
+
+  console.log({photoURL});
+
+
 
   return (
     <li>
       <div className="flex cursor-pointer items-center break-words p-4 hover:bg-[#e9eaeb]">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="https://avatars.githubusercontent.com/u/48112040?s=96&v=4"
+          src={photoURL}
           alt="Profile Picture"
           className="m-2 mr-4 h-10 w-10 rounded-full"
         />
